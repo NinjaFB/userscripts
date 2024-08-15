67cbcbc9a460e433e0f23cf57cefa56512f1c7e9
Oscar Ronberg <oscar@oronberg.com>
2024-08-15 10:21:42 +1000

    Create scienceflip.js

---
+++ scienceflip.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name        Scienceflip Image Fixer
+// @namespace   Violentmonkey Scripts
+// @match       https://www.scienceflip.com.au/*
+// @grant       none
+// @version     1.0
+// @author      Oscar Ronberg
+// @description 8/14/2024, 9:06:14 AM
+// ==/UserScript==
+
+const images = document.querySelectorAll("img[src^='https://drive.google.com']");
+
+images.forEach(image => {
+	const currentSrc = image.src;
+	const newSrc = currentSrc.replace("export=download", "sz=w1000").replace("uc", "thumbnail");
+
+	image.src = newSrc;
+});
